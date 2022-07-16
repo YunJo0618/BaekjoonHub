009037dfee3472a3c3574ae9af2260160a310cc1
YunJo0618 <104193872+YunJo0618@users.noreply.github.com>
2022-07-16 15:08:39 +0900

    [Bronze V] Title: 시험 성적, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,17 @@
+A= int(input())
+
+if 89 < A <=100:
+    print('A')
+
+elif 79 < A <=89 :
+    print('B')
+
+elif 69 < A <=79 :
+    print('C')
+
+elif 59 < A <=69 :
+    print('D')
+
+else:
+    print('F')
+        
